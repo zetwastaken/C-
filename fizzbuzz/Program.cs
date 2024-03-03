5c537441f1c343f33a13f63915d30db9d9e95be4
Zet <264193@student.pwr.edu.pl>
2024-03-03 23:56:03 +0100

    FizzBuzz

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿class fizzBuzz(){
+    public int lenghtFizz= 30;
+    public void writeFizzBuzz(){
+        for (int i = 1; i <= lenghtFizz; i++){
+            if (i % 3 == 0 && i % 5 == 0){
+                System.Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0){
+                System.Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0){
+                System.Console.WriteLine("Buzz");
+            }
+            else{
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
+class Program{
+    static void Main(string[] args){
+        fizzBuzz fb = new fizzBuzz();
+        fb.writeFizzBuzz();
+    }
+}
