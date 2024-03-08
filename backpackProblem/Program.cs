82671c86ab8a05ef986cd4ec22506bb8a43c7ed8
Zet <264193@student.pwr.edu.pl>
2024-03-08 08:39:42 +0100

    Works

---
+++ backpackProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace backpackProblem{
+    internal class Program{
+        static void Main(string [] args){
+            Problem problem = new Problem(10, 1);
+            Console.WriteLine(problem.ToString());
+            Result result = problem.solve();
+            Console.WriteLine("--------\n");
+            Console.WriteLine(result.ToString());
+        }
+
+    }
+}
--- fizzbuzz/obj/Debug/net8.0/fizzbuzz.AssemblyInfo.cs
+++ fizzbuzz/obj/Debug/net8.0/fizzbuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c537441f1c343f33a13f63915d30db9d9e95be4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4baaa8ae20766eb8a227e379ff1cbce03f4210")]
 [assembly: System.Reflection.AssemblyProductAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
