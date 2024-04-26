2a766ae6b617abec202d6d6281daf4edeb327be9
Zet <264193@student.pwr.edu.pl>
2024-04-26 21:09:09 +0200

    Fixed images2

--- fizzbuzz/obj/Debug/net8.0/fizzbuzz.AssemblyInfo.cs
+++ fizzbuzz/obj/Debug/net8.0/fizzbuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17441d54b163932e6ecef23b77038deef3e7fc6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c27ddefd6dedf84c91db1cb7df0ae2576d8322")]
 [assembly: System.Reflection.AssemblyProductAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
