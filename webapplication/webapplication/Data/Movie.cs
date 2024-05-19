de83cac4a704e853ac6c163b9ea71faa1a30d451
Zet <264193@student.pwr.edu.pl>
2024-05-19 22:02:23 +0200

    chore: Add Movie class to webapplication project

---
+++ webapplication/webapplication/Data/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace webapplication.Data
+{
+
+
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; }
+        public float? Rate { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
